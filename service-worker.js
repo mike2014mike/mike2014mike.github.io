c91ce570205ff03719f2d89bb7d9d1904b049f43
Mike Chen <mike2007.cv96g@gmail.com>
2018-07-26 10:44:37 +0800

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 //Cache polyfil to support cacheAPI in all browsers
 importScripts('./cache-polyfill.js');
 
-var cacheName = 'mikeBlog-20180726-v5';
+var cacheName = 'mikeBlog-20180726-v6';
 
 //Files to save in cache
 var files = [
