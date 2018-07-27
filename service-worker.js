703c29c8be1a83eca3d84b9d8d507bc8f7ece20a
Mike Chen <mike2007.cv96g@gmail.com>
2018-07-27 14:28:55 +0800

    CNAME
    mikechen.nctu.me

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 //Cache polyfil to support cacheAPI in all browsers
 importScripts('./cache-polyfill.js');
 
-var cacheName = 'mikeBlog-20180727-v7';
+var cacheName = 'mikeBlog-20180727-v8';
 
 //Files to save in cache
 var files = [
