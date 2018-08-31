d9905bcde4ada51f937516378df6d80ae9e2c12e
Mike Chen <mike2007.cv96g@gmail.com>
2018-08-31 15:01:01 +0800

    update

--- css/test.js
+++ css/test.js
@@ -32,19 +32,17 @@
 
 
 // ================== Hoisting 問題 ==================
-// 一開始只寫 console 的時候， a 是 not defined
+// // 一開始只寫 console 的時候， a 是 not defined
 // console.log(a);
 // var a = 3;
 // // 一旦後面寫個同名的 function，就會提升到前面，a 顯示為一個函式，function 優先權較高
 // function a() {
-
 // }
 
-// 如果改成 let ，會報錯，提示需要先宣告再使用
+// // 如果改成 let ，會報錯，提示需要先宣告再使用
 // console.log(b);
 // let b = 3;
 // function b() {
-
 // }
 
 
@@ -80,29 +78,29 @@
 // }
 
 // ================== 箭頭函式 ==================
-// 傳統寫法一
+// // 傳統寫法一
 // function abc() {
 //   return 'abc';
 // }
 
-// 傳統寫法二：避免 Hoisting 的 function 優先問題發生，程式相對穩定
+// // 傳統寫法二：避免 Hoisting 的 function 優先問題發生，程式相對穩定
 // let abc = function () {
 //   return 'abc';
 // }
 
-// 箭頭函式
+// // 箭頭函式
 // let abc = () => {
 //   return 'abc'
 // };
 
-// 大括號內如果只有一行可以省略
-// 箭頭函式預設會 return，所以 return 也省略
+// // 大括號內如果只有一行可以省略
+// // 箭頭函式預設會 return，所以 return 也省略
 // let abc = () => 'abc';
 
 // console.log(abc());
 
 
-// ES5 設定預設值寫法
+// // ES5 設定預設值寫法
 // function test1(a, b, c, d) {
 
 //   a = a || 'AAA'
@@ -114,7 +112,7 @@
 // }
 // console.log(test1());
 
-// ES6 預設值寫法
+// // ES6 預設值寫法
 // function test1_ES6(a = 'AAA', b = 'BBB', c = 'CCC', d = 'DDD') {
 //   console.log(a, b, c, d);
 // }
@@ -123,21 +121,93 @@
 
 
 
-// ES5 設定預設值寫法
-function test2(obj) {
+// // ES5 設定預設值寫法
+// function test2(obj) {
 
-  obj = obj || {}
-  obj.a = obj.a || 'AAA'
-  obj.b = obj.b || 'BBB'
-  obj.c = obj.c || 'CCC'
-  obj.d = obj.d || 'DDD'
+//   obj = obj || {}
+//   obj.a = obj.a || 'AAA'
+//   obj.b = obj.b || 'BBB'
+//   obj.c = obj.c || 'CCC'
+//   obj.d = obj.d || 'DDD'
 
-  console.log(obj);
+//   console.log(obj);
+// }
+// console.log(test2());
+
+// // ES6 預設值寫法 - 物件拆解
+// function test2_ES6({ a = 'AAA', b = 'BBB', c = 'CCC', d = 'DDD' }) {
+//   console.log(a, b, c, d);
+// }
+// console.log(test2_ES6({ a: 'Mike' }));
+
+
+
+
+
+
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// // let a = arr[0], b = arr[1], c = arr[2];
+
+// let [a, b, ...c] = arr;
+// // 頭兩個分給 a, b，剩下的都分給 c
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+// let mike = {
+//   name: 'Mike',
+//   bType: 'B',
+//   sex: 'male'
+// }
+
+// let { name, bType, sex } = mike;
+// // import { name, bType, sex } from 'mike';
+// // node_modules 常看到這種用法
+
+// console.log(name, bType, sex);
+
+
+// let name = 'Mike';
+// let sex = 'male';
+
+// // 傳統寫法：浪費人生，要打兩次？
+// let mike = {
+//   name: name,
+//   sex: sex
+// }
+// console.log(mike);
+
+// // ES6 寫法
+// let mike_ES6 = {
+//   name,
+//   sex
+// }
+// console.log(mike_ES6);
+
+
+
+function fn1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(1);
+      resolve(); // 做完事情要告訴他 resolve
+    }, 0);
+  })
 }
-console.log(test2());
 
-// ES6 預設值寫法 - 物件拆解
-function test2_ES6({ a = 'AAA', b = 'BBB', c = 'CCC', d = 'DDD' }) {
-  console.log(a, b, c, d);
+function fn2() {
+  console.log(2);
 }
-console.log(test2_ES6({ a: 'Mike' }));
+
+
+async function run() {
+  await fn1();
+  fn2();
+}
+
+run();
